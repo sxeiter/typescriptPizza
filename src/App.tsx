eacc158062f7ed34b362059da2769db18f1b631e
Pavel <sxeiter@mail.ru>
2024-01-26 15:45:17 +0500

    Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
-import {Routes, Route} from 'react-router-dom'; 
-import { Menu } from './Pages/Menu/Menu';
-import { Cart } from './Pages/Cart/Cart';
+
 
 function App() {
 	const [counter, setCounter] = useState<number>(0);
@@ -17,14 +15,6 @@ function App() {
 			<Button onClick={addCounter}>Кнопка</Button>
 			<Button appearence='big' onClick={addCounter}>Кнопка</Button>
 			<Input placeholder='email'/>
-			<div>
-				<a href='/'>Меню</a>
-				<a href='/cart'>Корзина</a>
-			</div>
-			<Routes>
-				<Route path='/' element={<Menu />} />
-				<Route path='/cart' element={<Cart />} />
-			</Routes>
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Cart } from './Pages/Cart/Cart.tsx';
+import { Menu } from './Pages/Menu/Menu.tsx';
+import { Error } from './Pages/Error/Error.tsx';
+import { Layout } from './layout/Menu/Menu.tsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Menu />
+			},
+			{
+				path: '/cart',
+				element: <Cart />
+			}
+		]
+
+	},
+	{
+		path: '*',
+		element: <Error />
+
+	}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<RouterProvider router={router}/>
 	</React.StrictMode>
 );
